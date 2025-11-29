67cd87bba516070cf22cc8b43b247cc62af489a1
unknown <kodanevhy>
2025-11-30 01:32:22 +0800

    alert

--- cbok/apps/alert/manager/google.py
+++ cbok/apps/alert/manager/google.py
@@ -6,9 +6,9 @@ import urllib.parse
 
 from bs4 import BeautifulSoup
 
-from alert import common
-from alert.login import google
-from alert.manager import base
+from cbok.apps.alert import common
+from cbok.apps.alert.login import google
+from cbok.apps.alert.manager import base
 
 LOG = logging.getLogger(__name__)
 
@@ -31,7 +31,7 @@ class GoogleAlertManager(base.BaseAlertManager):
         self.session.headers.update(common.HEADERS)
         # session_cookies = {cookie['name']: cookie['value'] for cookie in cookies}
         # print(session_cookies)
-        session_cookies = {'__Secure-1PSIDCC': 'AKEyXzXXpT9ulpUwNrhRHAoQbJTarZ0pM-G8Crj-lzfDhs_0k0e7IevXkWti5wM4Wg5b2hn5AA', 'SIDCC': 'AKEyXzVpSXsrHxRm13g6T0sBTcmkvDAwcD4b4Uv_QquCbcrvQaOpfnBpycpIaYiEMUzOYCmo', '__Secure-3PSIDTS': 'sidts-CjEB7pHptWQBKpfh646v_Ha2lWpTjSLsxv9IyeUbdLhmu7G9ZiFTOj_8es-1BlCNjf0TEAA', '__Secure-OSID': 'g.a000wAgf5-g10csqP8a6dO7YQun7sDOVHLPrBZx95VJy21opwagA9NNLg8oJrFzen2U7PSgpKwACgYKAVkSARISFQHGX2Mi6GKd1uUc_vzqmTHiQEJMxRoVAUF8yKpKvu5g-fU2PICqqtDtYoZn0076', 'OTZ': '8048882_24_24__24_', 'OSID': 'g.a000wAgf5-g10csqP8a6dO7YQun7sDOVHLPrBZx95VJy21opwagAZeQ4fldraQtliHFfdV873AACgYKASoSARISFQHGX2MikXiEiHaLAqP0uN2uFz3PxhoVAUF8yKrwdGhl9O-7LbS68Vjg0ro10076', '__Secure-3PAPISID': 'ZewdmDA5TFTkTray/A2jZZ-Xa7dkdQ9Bir', 'SAPISID': 'ZewdmDA5TFTkTray/A2jZZ-Xa7dkdQ9Bir', '__Secure-1PSIDTS': 'sidts-CjEB7pHptWQBKpfh646v_Ha2lWpTjSLsxv9IyeUbdLhmu7G9ZiFTOj_8es-1BlCNjf0TEAA', 'SID': 'g.a000wAgf5wFZgZIQc7Z1Q3RRDO1HKGIAyEEfdDy-cFK7nLQSfCV99f5w9XzSTeM1nfGlQKNTLgACgYKAQESARISFQHGX2Mi3g-wSa4SU4KTYldIYogv2hoVAUF8yKo3M2urDS3nGkI1wQ_cCfDI0076', '__Secure-3PSIDCC': 'AKEyXzWSTnpMFyBuU2qWb7fKnsl1gQEFoKE4Bbk2yVHsJuraMZdh-m4KeYlLfw7bK3doCPw', '__Secure-3PSID': 'g.a000wAgf5wFZgZIQc7Z1Q3RRDO1HKGIAyEEfdDy-cFK7nLQSfCV9tdNeo56EJUXjak6j1wU63AACgYKAVcSARISFQHGX2MiI1Rjh-XMaYmyJd8wKh-FiRoVAUF8yKq5ikja-YzDGL_g28oTEqLj0076', 'HSID': 'A2QBMStqIvox4N_bT', '__Secure-1PAPISID': 'ZewdmDA5TFTkTray/A2jZZ-Xa7dkdQ9Bir', 'APISID': 'IBsTHgbB49zy3mZZ/ALh1FpRXm2cjOc9yu', 'NID': '523=Ke5gIuwGtXI2mMqR2N0q-aoK4qfQRR-4C1_LLKpPHZhxnCTqcr5cmzr6AblxO3ckzoV02110ZR1-YY_guISbRms0gY8J_vz7OMKfji5KtKQObBc7rmfIB_VunJc5uYNJsgpGr7BgPkBz0X9QaVAmd_P8nuqVM5hUgwnNtNzhU6WaorwbpUi_qF5Qt7Jzrs_cy79QeULytX5jReF4ALx05A8xt4t3oXlnn--51hJ0vPGsv8e4QSUMhnsoZuVEYfEO0boXnTX4Wt-Lu9uG1cIM_7kxyuwoap0hBNi2dhYFUhce9Cuyp-4fXBfpKj4V94rfwWKVOKmZyAe9D7zVEAGH9JBKSG3s0C8iFPBkpPnQ2bfkUdh35iEV4uNXRRosYrfvZX_6ZJ-Rin4nvMXE7bClEVruANPm8qgY8byc0mbMos1efA-xI_DUcRPJvP12Xn6gmfBHnC4C4Bv_lNzW7BeYDxwf5G0zge-cGEtZyg0-X1xXKAzTGhP4BgV9w4h9FCTewO0OnXgcLwse5Ia56o7WAs4tj4Yg_bEpI_Q1yjOybhcgRZaaPcK7CqfGrdqZ9m7jYQB8rpUCcx01k0N2Cs1qBeFcFmliz39W_I1Ms0oSSjHu3pC4jm27ol9XWh8z29TnpxsPJRbtZklSfOHxZFk_Sy7wpeRLrjtJEO6U', 'SSID': 'A56SLulMZrnwzzYYd', '__Secure-1PSID': 'g.a000wAgf5wFZgZIQc7Z1Q3RRDO1HKGIAyEEfdDy-cFK7nLQSfCV9eLCujJ3mQQSwhnDv-91d0wACgYKAb4SARISFQHGX2Mi-p56cXjhVkm-nR4PUWOgXxoVAUF8yKr_gFLJx5uXGU2cJL0RZMLA0076'}
+        session_cookies = {'__Secure-3PSIDCC': 'AKEyXzVs9E_1kQVHXrui3X0M0A0LuDg7OAEMfld5o_0McaOMDPFhKuYlITgBYO909d9bQQRd4g', '__Secure-1PSIDCC': 'AKEyXzVRUueu91YOJqmF1nLmsM6anFEVLDK6xXj-Djav3xE_xGVCENESoLqQBdIZryCPhW1x5g', 'SIDCC': 'AKEyXzXe4nD7mkMORwAbtbHZM1wzjUyl7JYwdZ42buCtTeX6beTSXYjUOh3qA0rH-ItTCmN36A', 'APISID': '-NzkO6mo-k2Ty5ii/ABjvRswadFDoG0gM3', 'SSID': 'ACq9kWmdgJa-bYS8-', 'NID': '526=VBw_VXjxZ0ar8GWr0yHpb0xZNmpxbDs70Aa0_dHtkWww-3inXkmcGNhvOcZ6OwvCi1gzaucNiqdJPlKMxyRylcnFpo0ywoKwHLHEwyD_13mmJXUkHhAZHewFb4Z5C1TTaEmdkcXIQyE8c7uSm02YlFerVzegye61CwPx_zgsl9rC4UXuvz6YcFG5o1WfKiM3G_xEc9ZP7UIUdXJ3paOytdCb4i4tZc8oDUtYKiX6VOxCznjaLUmzY-anIZy9C2SuEkZPA0ekAobxL4YxRff6dDCFag8zRtfotXu5b2ftE8o3-rasHFUv4xKCbgGqggRy0Dx2VGNu7nqPaYw4RvATJORftGOZzBi66iFttCygZfFoBUzIp68MFCBrsX8p8Qk9AMNg89E2RinMdf6EhIz9dC4G5z9V8oyZ-zS7PRX3QnG6w0R9bSEmyTb_8nw7m-1YLEmkTAl5MjSbok5woE2J3ZD3N8UIqytbx3hMxOh32QxyQ5XEwfcWlCjPa9B6ec2e9JGjwV5gBm4zW47O6GhTmpe7P-mF2sL85AEyfnDc-Ba2k0WOdnA8zmqE5RK5qt6YwSrfIyNu0csVNMVF40yBC0eG3KGgCjz62wMSEZc13h7VLLRZ2zOgVl90sxRMyn5nZXM7DkWUlj1O4QuAFyCoX5sUl28S7oFeL11rYQkOYw', '__Secure-3PSID': 'g.a0004Agf58wkvNaNEZTuk8uWEBiOeqd0CVngAa-MwCRjB2YzBkoZhqGN_WD5ea07Y54TsC0huwACgYKAegSARISFQHGX2MibRO_Z6MQPc4Qt57FGELeaxoVAUF8yKrj3CWzpAk43tWusvaQ7M3c0076', '__Secure-1PSID': 'g.a0004Agf58wkvNaNEZTuk8uWEBiOeqd0CVngAa-MwCRjB2YzBkoZ2fq9j3QopmuuvBn4TvUlDgACgYKATISARISFQHGX2Mi6SCRskWhF88tlYdqxipYqRoVAUF8yKqTqRGdNR4ZCBTV_3WZj6th0076', 'OTZ': '8368745_24_24__24_', '__Secure-3PAPISID': 'PBw-cY9-eVCCT8Wv/AtUQ5IMKJu0JUCoOD', '__Secure-1PAPISID': 'PBw-cY9-eVCCT8Wv/AtUQ5IMKJu0JUCoOD', 'SID': 'g.a0004Agf58wkvNaNEZTuk8uWEBiOeqd0CVngAa-MwCRjB2YzBkoZwdf22q3d8pc_EkpZDLv_swACgYKAWgSARISFQHGX2MiGT17UY5_h9vrwf1ymNKlmRoVAUF8yKqYKSkbqAJ9zdwhVer_mh5C0076', 'SAPISID': 'PBw-cY9-eVCCT8Wv/AtUQ5IMKJu0JUCoOD', 'HSID': 'Av1XRhLElgTnv_vxP'}
 
         page = self.session.get(self.INDEX, cookies=session_cookies)
         if page.status_code != 200:
@@ -52,64 +52,72 @@ class GoogleAlertManager(base.BaseAlertManager):
                         alert_name = a[1][2][0]
                         alert_id = a[1][5][0][-1]
                         r.update({alert_name: alert_id})
-                return r
+                return r, session_cookies
             except SyntaxError:
-                print("解析失败，可能是数据格式不符合 Python 语法。")
+                raise
         else:
-            print("未找到 window.STATE 字段。")
+            print("window.STATE field not found")
+            raise
 
 
-    def analysis_history(self, name):
+    def analysis_history(self, name, date=7):
         now = int(time.time())
-        # Based data from the first 7 days, I do not recommend
-        # you to turn it up, cuz it's usually useless
-        d = 604800 * 1
+        d = 86400 * date
+        # That mean the past `date` days by default 
         params = f'[null,null,{now},{d}]'
-        sid = self.analysis_index()[name]
+        indexed, session_cookies = self.analysis_index()
+        sid = indexed[name]
         url = f'{self.HISTORY}?params={urllib.parse.quote(params)}&s={sid}'
-        session_cookies = {'__Secure-1PSIDCC': 'AKEyXzXhaMIooZbRsJzQni-Emz2V7z_go7qGnsYatjyPTyv0e9uGo-kYjatAVQ1UbblrjsIT', 'SIDCC': 'AKEyXzXoWt2he7PEcIkMi-F028BCE1anOcUt-lwkbahYle-UtSx_Xic66zdqKOOTwt4G2OZhlA', '__Secure-3PSIDTS': 'sidts-CjEB7pHptYMJL7LlMloMZ2Y7PXQ2p0ZMov_maF-uQGuGZJJGVe3XNsufgARcn91ZcwLlEAA', '__Secure-OSID': 'g.a000wAgf54x5w4eV8eP6F2zw5KE2KXFFhiu_me1WTu01oLieIoHS9U9tW5IQxXLSWzcnSsYcqQACgYKAWMSARISFQHGX2MizBmRKxN8HmJNWGXrYxRuqBoVAUF8yKrSUVFioJDJaKw91xOSQ-J10076', 'OTZ': '8048921_24_24__24_', 'OSID': 'g.a000wAgf54x5w4eV8eP6F2zw5KE2KXFFhiu_me1WTu01oLieIoHSxUG6-ThOdglaoy2rrn73hQACgYKAUoSARISFQHGX2MioF-UfiiyvYb-ZB_XYDo_CxoVAUF8yKoBvbpd7_F51NRm1CQ6JWfY0076', '__Secure-3PAPISID': 'WJpxO0qol6873PJf/ASNrXbdWDRKj0qE8G', 'SAPISID': 'WJpxO0qol6873PJf/ASNrXbdWDRKj0qE8G', '__Secure-1PSIDTS': 'sidts-CjEB7pHptYMJL7LlMloMZ2Y7PXQ2p0ZMov_maF-uQGuGZJJGVe3XNsufgARcn91ZcwLlEAA', 'SID': 'g.a000wAgf5xBEtPItfG2JFEvF9szskOVLMUfcQ9Fz-uD3NKSYQN6JmvclRYaymZHNRZh4ePS_5wACgYKASYSARISFQHGX2MiHGZ19358qme06Kz08L3gaRoVAUF8yKpiZPcLGXCcgr1rbTQrmSQu0076', '__Secure-3PSIDCC': 'AKEyXzWkAwNgNuGRFgRHdWDcYvTEUawcO78FjHWrqwjMN_I7pvWUaMlkDTfYd2BrkYrcSJZTng', '__Secure-3PSID': 'g.a000wAgf5xBEtPItfG2JFEvF9szskOVLMUfcQ9Fz-uD3NKSYQN6JVobDK7uBd8bkEO1R2lro1wACgYKAfMSARISFQHGX2MiCtqg5LO1mZNhFlTobdGIFBoVAUF8yKqTsF6ejItLQQSaHp5xL17C0076', 'HSID': 'AWnr_2tg5_Y53jkmh', '__Secure-1PAPISID': 'WJpxO0qol6873PJf/ASNrXbdWDRKj0qE8G', 'APISID': 'O_lHLn3Zva6IlT2h/AOpas3rQKDM2v9xAj', 'NID': '523=cmIa_aCITzlVRL1ClITQTimijN9DlbYkKLTD3azCLk6hpAlOjlIQ17d02-NyBBsXmTnKfb0RcyOEiAeCoi1APFYNYHRNTYnqRQz4DN11lDQxxHpkC5XeCMi6ZBZaKHU7acuJASUV1wDVQEL--Bia0Uonv6EBP1O-bQr3D8Zp6ETRbhVeKPfpHwTfDqkOyeptJLkhJ4mNtpoMgBMM09RqFJ2VTPIJOp_YsmQR3PDs2UatusZiWObdNCFwAKQFjbuE84AX-wCWxeSCXcNzHd6MjPmChrKFtkQodavZ5xjBD-alS2fB3FzCunBz5ydp4HykPz02DD69y0TcMo5Md-n2fqqNw0DmhbT__e4wP9KQsHJv_RrBDwd1ShA0hoL6Z1qd2I8weKGLwscQW-F8v_VA5rSJwNttKNoLk40Z5UmswNpQjukk56uBmw4UnPhc_YZdCPNQ1QlSbL5FpEUo30E3vqXeTXTU4_4z3nrW2VdIj4QnBwj4N_zBZ1jYIK_ai8s1Eb35h-5SWkXArIcS7G4TjR7XjpX5koteE-tU6jVpPQ7sCEl7f4cmLhuRsjjgNrAwqR1rPYd9js2tTomW4g-U83W7AK63fAhNkPMhD8OekNLKI-G6DbjmUmaZFNQKwU1UF7io-gkW6CUxGlPTnE_f3-qm4Y-4-ZVSU7TZ', 'SSID': 'A7l7czY1LzRrexjBI', '__Secure-1PSID': 'g.a000wAgf5xBEtPItfG2JFEvF9szskOVLMUfcQ9Fz-uD3NKSYQN6JQ72UJ2KiGY23A_d7ZSdV-wACgYKAVkSARISFQHGX2MiWej5KC_nSwCCT5-OQ0ZGARoVAUF8yKrkv-qJ1xigeDKxcLap5GAW0076'}
-
+        
         r = self.session.get(url, cookies=session_cookies)
-        # 先取前七天作为基础数据，然后再一天一天地取
 
         html = r.text.encode('utf-8')
 
+        import pdb; pdb.set_trace()
         soup = BeautifulSoup(html, 'html.parser')
+
         results = []
-
-        history_divs = soup.find_all("div", class_="history_message")
-
-        for div in history_divs:
-            timestamp = div.get("data-timestamp", "")
-            items = div.find_all("li", class_="result")
-
-            for item in items:
-                title_tag = item.find("a", class_="result_title_link")
-                source_tag = item.find("div", class_="result_source")
-                snippet_tag = item.find("span", class_="snippet")
-
-                if title_tag and snippet_tag and source_tag:
-                    title = title_tag.get_text(strip=True).replace('\xa0', '')
-                    snippet = snippet_tag.get_text(strip=True).replace('\xa0', '')
-                    source = source_tag.get_text(strip=True).replace('\xa0', '')
-
-                    result = {
-                        "timestamp": timestamp,
-                        "title": title,
-                        "link": title_tag.get("href"),
-                        "snippet": snippet,
-                        "source": source
-                    }
-                    results.append(result)
-
-        # 打印结果
-        for r in results:
-            print(r)
-        print(len(results))
-        from datetime import datetime
-        print(datetime.fromtimestamp(1743598729))
+        
+        for message in soup.find_all('div', class_='history_message'):
+            date_elem = message.find('span', class_='age')
+            date = date_elem.text.strip() if date_elem else 'unknown'
+            
+            source_type_elem = message.find('h3', class_='source')
+            source_type = source_type_elem.text.strip() if source_type_elem else 'unknown'
+            
+            for result in message.find_all('li', class_='result'):
+                title_elem = result.find('a', class_='result_title_link')
+                source_elem = result.find('div', class_='result_source')
+                snippet_elem = result.find('span', class_='snippet')
+                
+                if not title_elem or not snippet_elem:
+                    continue
+                    
+                title = title_elem.text.strip()
+                url = title_elem.get('href', '')
+                source = source_elem.text.strip() if source_elem else ''
+                snippet = snippet_elem.text.strip()
+                
+                keywords = []
+                if snippet_elem:
+                    bold_elems = snippet_elem.find_all('b')
+                    keywords = [b.text.strip() for b in bold_elems if b.text.strip()]
+                
+                results.append({
+                    'date': date,
+                    'type': source_type,
+                    'title': title,
+                    'source': source,
+                    'snippet': snippet,
+                    'url': url,
+                    'keywords': keywords
+                })
+        
+        return results
 
 
 if __name__ == '__main__':
     test = GoogleAlertManager()
-    test.analysis_history('乒乓球')
+    result = test.analysis_history('王楚钦')
+    print(result)
+    print(len(result))
